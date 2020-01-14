6cfb1283c76d6cbd45156511121b397cdd75e216
Devon Peticolas <devon@oden.io>
2020-01-14 17:01:06 -0500

    fix project in connections.sh

--- script/connections.sh
+++ script/connections.sh
@@ -9,7 +9,7 @@ airflow connections --add \
     --conn_type=google_cloud_platform \
     --conn_extra='{
         "extra__google_cloud_platform__key_path": "",
-        "extra__google_cloud_platform__project": "oden-qa",
+        "extra__google_cloud_platform__project": "${GCLOUD_PROJECT}",
         "extra__google_cloud_platform__scope": ""
     }'
 
@@ -18,7 +18,7 @@ airflow connections --add \
 airflow connections --delete --conn_id docker_default
 airflow connections --add \
     --conn_id=docker_default \
-    --conn_uri="docker://oauth2accesstoken:$(gcloud auth application-default print-access-token)@gcr.io/oden-qa"
+    --conn_uri="docker://oauth2accesstoken:$(gcloud auth application-default print-access-token)@gcr.io/${GCLOUD_PROJECT}"
 
 
 # Connect to staging clousql instance via a proxy with a regular postgres connection
@@ -32,4 +32,4 @@ airflow connections --add \
 airflow connections --delete --conn_id google_cloud_sql
 airflow connections --add \
   --conn_id=google_cloud_sql \
-  --conn_uri="gcpcloudsql://airflow:${CLOUDSQL_POSTGRES_PASSWORD}@1.1.1.1:3306/postgres?database_type=postgres&project_id=oden-qa&location=${GCLOUD_REGION}&instance=${CLOUDSQL_POSTGRES_DB_NAME}&use_proxy=True&sql_proxy_use_tcp=False"
+  --conn_uri="gcpcloudsql://airflow:${CLOUDSQL_POSTGRES_PASSWORD}@1.1.1.1:3306/postgres?database_type=postgres&project_id=${GCLOUD_PROJECT}&location=${GCLOUD_REGION}&instance=${CLOUDSQL_POSTGRES_DB_NAME}&use_proxy=True&sql_proxy_use_tcp=False"
